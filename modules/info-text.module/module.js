6ba7e3140cd331d62bc86c817d4b8b084381924a
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-12-23 14:19:46 +0100

    Add info text module

---
+++ modules/info-text.module/module.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+  el: '#info-text',
+  data: {
+    title: 'We are interested in buying your house',
+    text: 'Do you own an unwanted house? We are in the business of assisting people who want or need to sell their house fast whether you are behind on payments, facing foreclosure, bankruptcy, needing repair, divorce or setting an estate sale. We provide fair and amicable solutions as quickly as needed. We will buy yout house in any condition for a fair price and allow you to move on the date of your choice.'
+  }
+})
